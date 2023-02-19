b963b4638a7ba374339e6b7b0c368fd9532edab0
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-02-18 19:14:33 -0500

    so pa ter a certeza

--- SRC/sort_pt.c
+++ SRC/sort_pt.c
@@ -6,38 +6,11 @@
 /*   By: miandrad <miandrad@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 14:50:53 by miandrad          #+#    #+#             */
-/*   Updated: 2023/02/17 17:32:46 by miandrad         ###   ########.fr       */
+/*   Updated: 2023/02/18 10:01:21 by miandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-
-// void	sort(t_list **a, t_list **b)
-// {
-// 	int	flag;
-
-// 	while (*a)
-// 	{
-// 		flag = 0;
-// 		if (*b && *(*b)->content > *(*a)->content)
-// 			ra(a, 0);
-// 		while (*b && *(*b)->content > *ft_lstlast(*a)->content)
-// 		{
-// 			pa(a, b);
-// 			flag = 1;
-// 		}
-// 		if (flag == 1)
-// 			rra(a, 0);
-// 		pb(a, b);
-// 		if ((*b)->next != NULL && reverse_order(*b) && check_order(*a))
-// 		{
-// 			break ;
-// 		}
-// 	}
-// 	while (*b)
-// 		pa(a, b);
-// }
-
 
 void	moves_reset(t_moves *moves)
 {
